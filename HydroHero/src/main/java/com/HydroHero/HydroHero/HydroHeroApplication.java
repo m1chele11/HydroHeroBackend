7200ef64f4a10802a85b61e79b88209e6f08b791
michele <mbarrant@iu.edu>
2024-07-19 13:53:54 -0400

    initialization

---
+++ HydroHero/src/main/java/com/HydroHero/HydroHero/HydroHeroApplication.java
@@ -0,0 +1,13 @@
+package com.HydroHero.HydroHero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HydroHeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HydroHeroApplication.class, args);
+	}
+
+}
---
+++ HydroHero/src/test/java/com/HydroHero/HydroHero/HydroHeroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HydroHero.HydroHero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HydroHeroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
